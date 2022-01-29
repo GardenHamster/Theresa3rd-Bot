821e26cc642d4120a9ae423480ac571466482b5b
GardenHamster <zqn260256@gmail.com>
2022-01-29 23:45:36 +0800

    修正pixiv动图转gif失败的问题

--- Theresa3rd-Bot/Util/ZipHelper.cs
+++ Theresa3rd-Bot/Util/ZipHelper.cs
@@ -24,7 +24,7 @@ namespace Theresa3rd_Bot.Util
                 {
                     string filename = Path.GetFileName(fileEntry.Name);
                     if (string.IsNullOrEmpty(filename)) continue;
-                    string fullFilePath = Path.Combine("unZipDirPath", filename);
+                    string fullFilePath = Path.Combine(unZipDirPath, filename);
                     streamWriter = File.Create(fullFilePath);
                     byte[] buffer = new byte[2048];
                     int size = zipInputStream.Read(buffer, 0, 2048);

894cb1be9415a18c17b6135bc2b43b09c7182dd7
GardenHamster <zqn260256@gmail.com>
2022-06-20 13:52:48 +0800

    以图搜图功能

---
+++ Theresa3rd-Bot/Handler/SaucenaoHandler.cs
@@ -0,0 +1,28 @@
+﻿using Mirai.CSharp.HttpApi.Models.EventArgs;
+using Mirai.CSharp.HttpApi.Session;
+using System.Threading.Tasks;
+using Theresa3rd_Bot.Business;
+
+namespace Theresa3rd_Bot.Handler
+{
+    public class SaucenaoHandler
+    {
+        private PixivBusiness pixivBusiness;
+
+        public SaucenaoHandler()
+        {
+            pixivBusiness = new PixivBusiness();
+        }
+
+        public async Task searchSource(IMiraiHttpSession session, IGroupMessageEventArgs args, string message)
+        {
+
+        }
+
+
+
+
+
+
+    }
+}

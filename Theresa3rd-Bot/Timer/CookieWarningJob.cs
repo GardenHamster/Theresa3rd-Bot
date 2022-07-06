abba406de160f7470c4a662ff39e3e0bda7e7b2c
GardenHamster <zqn260256@gmail.com>
2022-07-06 15:00:55 +0800

    cookie过期提醒

---
+++ Theresa3rd-Bot/Timer/CookieWarningJob.cs
@@ -0,0 +1,50 @@
+﻿using Mirai.CSharp.HttpApi.Models.ChatMessages;
+using Mirai.CSharp.Models.ChatMessages;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Theresa3rd_Bot.Common;
+using Theresa3rd_Bot.Model.Config;
+using Theresa3rd_Bot.Model.PO;
+using Theresa3rd_Bot.Util;
+
+namespace Theresa3rd_Bot.Timer
+{
+    public class CookieWarningJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            try
+            {
+                await CheckAndWarn(BotConfig.WebsiteConfig.Pixiv, 2, "Pixiv Cookie");
+            }
+            catch (Exception ex)
+            {
+                LogHelper.Error(ex, "CookieWarningJob异常");
+            }
+        }
+
+        private async Task CheckAndWarn(WebsitePO website, int diffDay, string cookieName)
+        {
+            try
+            {
+                DateTime expireDate = website.CookieExpireDate;
+                if (DateTime.Now.AddDays(diffDay) < expireDate) return;
+                if (expireDate.AddDays(diffDay) < DateTime.Now) return;
+                string warnMessage = $"{cookieName}将在{expireDate.ToString("yyyy-MM-dd HH:mm:ss")}过期，请尽快更新cookie";
+                foreach (long memberId in BotConfig.PermissionsConfig.SuperManagers)
+                {
+                    await MiraiHelper.Session.SendFriendMessageAsync(memberId, new PlainMessage(warnMessage));
+                }
+            }
+            catch (Exception ex)
+            {
+                LogHelper.Error(ex, "CookieWarningJob.CheckAndWarn异常");
+            }
+        }
+
+
+    }
+}

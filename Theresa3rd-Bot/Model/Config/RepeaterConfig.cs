f913cc4c55e305d3e83a745e6ea27b9ab1690c42
GardenHamster <zqn260256@gmail.com>
2022-09-20 15:11:20 +0800

    回滚

--- Theresa3rd-Bot/Model/Config/RepeaterConfig.cs
+++ Theresa3rd-Bot/Model/Config/RepeaterConfig.cs
@@ -11,6 +11,7 @@ namespace Theresa3rd_Bot.Model.Config
 
         public int RepeatTime { get; set; }
 
+        public int RepeatMode { get; set; }
         public RepeaterConfig()
         {
             this.RepeatTime = 3;

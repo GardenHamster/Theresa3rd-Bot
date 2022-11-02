8cc2b6bfb11796c84571a3c936dfb705a9fb574e
GardenHamster <zqn260256@gmail.com>
2022-11-02 15:12:11 +0800

    更新版本号

--- Theresa3rd-Bot/Common/BotConfig.cs
+++ Theresa3rd-Bot/Common/BotConfig.cs
@@ -8,7 +8,7 @@ namespace Theresa3rd_Bot.Common
 {
     public static class BotConfig
     {
-        public const string BotVersion = "v0.7.3";
+        public const string BotVersion = "v0.7.4";
 
         public static DBConfig DBConfig = new DBConfig();
 

deb40d9e5b7874b2b089326a2870665b416b183b
GardenHamster <zqn260256@gmail.com>
2023-05-02 18:01:38 +0800

    消息缓存和词云相关数据库表

---
+++ Theresa3rd-Bot/TheresaBot.Main/Model/PO/MessageRecordPO.cs
@@ -0,0 +1,32 @@
+﻿using SqlSugar;
+
+namespace TheresaBot.Main.Model.PO
+{
+    [SugarTable("message_record")]
+    public class MessageRecordPO : BasePO
+    {
+        [SugarColumn(IsNullable = false, ColumnDescription = "消息ID")]
+        public long MsgId { get; set; }
+
+        [SugarColumn(IsNullable = false, ColumnDescription = "发送群ID")]
+        public long GroupId { get; set; }
+
+        [SugarColumn(IsNullable = false, ColumnDescription = "发送人ID")]
+        public long MemberId { get; set; }
+
+        [SugarColumn(IsNullable = false, ColumnDescription = "bot发出的消息")]
+        public bool SendOut { get; set; }
+
+        [SugarColumn(IsNullable = false, Length = 1000, ColumnDescription = "发送内容")]
+        public string Message { get; set; }
+
+        [SugarColumn(IsNullable = false, ColumnDescription = "发送时间")]
+        public DateTime CreateDate { get; set; }
+
+        [SugarColumn(IsNullable = false, Length = 200, ColumnDescription = "包含的图片地址")]
+        public string ImageUrls { get; set; }
+
+        [SugarColumn(IsNullable = false, Length = 50, ColumnDescription = "包含的PixivId")]
+        public string PixivId { get; set; }
+    }
+}
---
+++ Theresa3rd-Bot/TheresaBot.Main/Model/PO/WordCloudPO.cs
@@ -0,0 +1,26 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheresaBot.Main.Model.PO
+{
+    [SugarTable("word_cloud")]
+    public class WordCloudPO : BasePO
+    {
+        [SugarColumn(IsNullable = false, ColumnDescription = "群ID")]
+        public long GroupId { get; set; }
+
+        [SugarColumn(IsNullable = false, Length = 20, ColumnDescription = "关键词")]
+        public string Word { get; set; }
+
+        [SugarColumn(IsNullable = false, Length = 20, ColumnDescription = "权重")]
+        public double Weight { get; set; }
+
+        [SugarColumn(IsNullable = false, ColumnDescription = "添加日期")]
+        public DateTime CreateDate { get; set; }
+
+    }
+}

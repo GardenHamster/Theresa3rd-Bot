ea6ad852da8fa3fc4d98d400fd15ba14292b8cc9
ZQN <zqn260256@gmail.com>
2023-10-25 15:14:33 +0800

    Update BasePluginConfig.cs

--- Theresa3rd-Bot/TheresaBot.Main/Model/Config/BasePluginConfig.cs
+++ Theresa3rd-Bot/TheresaBot.Main/Model/Config/BasePluginConfig.cs
@@ -1,7 +1,10 @@
-﻿namespace TheresaBot.Main.Model.Config
+﻿using YamlDotNet.Serialization;
+
+namespace TheresaBot.Main.Model.Config
 {
     public abstract record BasePluginConfig : BaseConfig
     {
+        [YamlMember(Order = -100)]
         public bool Enable { get; set; }
     }
 }
